5aa7325e94c1ef7e6def6b0b805b1dbda600f646
Yuting Wang <yuting.wang@sjtu.edu.cn>
2025-04-19 16:50:55 +0800

    Updated client.c

--- CompCertOC/examples/running_example/client.c
+++ CompCertOC/examples/running_example/client.c
@@ -16,19 +16,19 @@ int main(void)
 	pthread_t a;
 
 	int input[N] = {1, 2, 3, 4, 5}, result[N];
-	int mask = 0;
+	int mask = 0, i;
 	Arg arg = {input, result, N};
     
 	pthread_create(&a, NULL, server, &arg);
 
-	for (int i = 0; i < N; i++) {
+	for (i = 0; i < N; i++) {
 		mask += input[i];
 		// yield();
 	}
 
 	pthread_join(a, NULL);
 
-	for (int i = 0; i < N; i++) {
+	for (i = 0; i < N; i++) {
 		result[i] = result[i] & mask;
 		printf("%d; ", result[i]);
 	}
